d12f78ce4e71e6c38407f4624eec6676fbe09954
TRosa Dev <thiagosrosa@gmail.com>
2023-11-13 23:16:58 -0300

    feat: create dummy List<Expense>

--- lib/widgets/expenses/index.dart
+++ lib/widgets/expenses/index.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
 
 class Expenses extends StatefulWidget {
@@ -10,6 +11,21 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'Flutter Course',
+      amount: 19.99,
+      category: Category.work,
+      date: DateTime.now(),
+    ),
+    Expense(
+      title: 'Cinema',
+      amount: 15.69,
+      category: Category.leisure,
+      date: DateTime.now(),
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
