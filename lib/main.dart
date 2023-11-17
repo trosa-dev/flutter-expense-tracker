4187fe5d2b0d8f8998840e7e9145d43c8de4e908
TRosa Dev <thiagosrosa@gmail.com>
2023-11-17 10:07:00 -0300

    feat: add AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Expenses(),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: const Expenses(),
     );
   }
 }
