aad7f6d11136f468c7f6ccec3f520dc37f2253b7
TRosa Dev <thiagosrosa@gmail.com>
2023-11-20 11:20:17 -0300

    feat: create _openAddExpenseOverlay

--- lib/widgets/expenses/index.dart
+++ lib/widgets/expenses/index.dart
@@ -27,6 +27,11 @@ class _ExpensesState extends State<Expenses> {
     ),
   ];
 
+  void _openAddExpenseOverlay() {
+    showModalBottomSheet(
+        context: context, builder: (ctx) => const Text('Modal bottom sheet'));
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,7 +39,7 @@ class _ExpensesState extends State<Expenses> {
         title: const Text('Flutter ExpenseTracker'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _openAddExpenseOverlay,
             icon: const Icon(Icons.add),
           )
         ],
