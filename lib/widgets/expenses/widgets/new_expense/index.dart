ee7cf2153fb9da8a9ccbc25567caad4cb15f3caf
TRosa Dev <thiagosrosa@gmail.com>
2023-11-22 00:12:52 -0300

    feat: add controller at _NewExpenseState

--- lib/widgets/expenses/widgets/new_expense/index.dart
+++ lib/widgets/expenses/widgets/new_expense/index.dart
@@ -10,18 +10,34 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.all(16),
+    return Padding(
+      padding: const EdgeInsets.all(16),
       child: Column(
         children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
               label: Text('Title'),
             ),
           ),
+          Row(
+            children: [
+              ElevatedButton(
+                  onPressed: () => print(_titleController.text),
+                  child: const Text('Save Expense')),
+            ],
+          )
         ],
       ),
     );
