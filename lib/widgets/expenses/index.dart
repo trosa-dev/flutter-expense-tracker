b7064035220d7683800af6d15e6cf6e8128a60bd
TRosa Dev <thiagosrosa@gmail.com>
2023-12-03 15:34:46 -0300

    feat: add Dismissible Widget to expenses_list

--- lib/widgets/expenses/index.dart
+++ lib/widgets/expenses/index.dart
@@ -41,6 +41,12 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void _removeExpense(Expense expense) {
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -57,7 +63,10 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(expenses: _registeredExpenses),
+            child: ExpensesList(
+              expenses: _registeredExpenses,
+              onRemoveExpense: _removeExpense,
+            ),
           )
         ],
       ),
--- lib/widgets/expenses/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses/widgets/expenses_list/expenses_list.dart
@@ -3,15 +3,26 @@ import 'package:expense_tracker/widgets/expenses/widgets/expenses_list/widgets/e
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({
+    super.key,
+    required this.expenses,
+    required this.onRemoveExpense,
+  });
 
   final List<Expense> expenses;
+  final void Function(Expense expense) onRemoveExpense;
 
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: expenses.length,
-      itemBuilder: (ctx, index) => ExpenseItem(expense: expenses[index]),
+      itemBuilder: (ctx, index) => Dismissible(
+        onDismissed: (direction) {
+          onRemoveExpense(expenses[index]);
+        },
+        key: ValueKey(expenses[index]),
+        child: ExpenseItem(expense: expenses[index]),
+      ),
     );
   }
 }
