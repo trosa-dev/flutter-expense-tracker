667860fbbb20746e6b75aec2bc9d88d5a51139b6
TRosa Dev <thiagosrosa@gmail.com>
2023-12-03 18:26:55 -0300

    style: using theme styles

--- lib/main.dart
+++ lib/main.dart
@@ -36,7 +36,7 @@ class MyApp extends StatelessWidget {
         ),
         textTheme: ThemeData().textTheme.copyWith(
               titleLarge: TextStyle(
-                fontWeight: FontWeight.normal,
+                fontWeight: FontWeight.bold,
                 color: kColorScheme.onSecondaryContainer,
                 fontSize: 14,
               ),
